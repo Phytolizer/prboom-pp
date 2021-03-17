a7e5def76ed5c245b53a7c8578f61473c465132d
kyle.coffey <kyle.coffey@tamu.edu>
2021-03-17 07:07:04 -0500

    It's no longer called DSDA Doom.

--- prboom2/src/SDL/i_system.cc
+++ prboom2/src/SDL/i_system.cc
@@ -216,7 +216,7 @@ unsigned long I_GetRandomTimeSeed(void)
  */
 const char *I_GetVersionString(char *buf, size_t sz)
 {
-    snprintf(buf, sz, "%s v%s (https://github.com/kraflab/dsda-doom/)",
+    snprintf(buf, sz, "%s v%s (https://github.com/onContentStop/prboom-pp/)",
              PACKAGE_NAME, PACKAGE_VERSION);
     return buf;
 }
